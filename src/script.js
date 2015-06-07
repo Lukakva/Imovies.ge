49cb2886fe3a3b3d35dc70f2b05f124f4e82493a
Lukakva <lukakvavilashvili@hotmail.com>
2015-06-07 21:38:20 +0400

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,22 @@
+jQuery(window).ready(function(){
+	setTimeout(function(){
+		var socialButtons = jQuery("#sociallinks").find("div")[0];
+		var newButton = document.createElement("a");
+		newButton.innerHTML = "áƒ’áƒáƒ“áƒ›áƒáƒ¬áƒ”áƒ áƒ";
+		var imoviesVideoPlayer = jQuery("video");
+		// on some computers there are 3 video tags (and not 1 like on mine)
+		imoviesVideoPlayer = imoviesVideoPlayer.length > 1 ? imoviesVideoPlayer[2] : imoviesVideoPlayer[0];
+		imoviesVideoPlayer.addEventListener("durationchange",function(){
+			if(imoviesVideoPlayer.duration > 20){
+				jQuery(newButton).attr("style","background-color:#299ecc;border-radius: 3px;padding-top:0px;margin-top:0px;height: 19px;color:white");
+				jQuery(newButton).attr({
+					"href": imoviesVideoPlayer.src,
+					"style": "background-color:#299ecc;border-radius: 3px;padding-top:0px;margin-top:0px;height: 19px;color:white",
+					"download": jQuery(".film_title.notbold.blue.film_title_eng").html(),
+					"class": "button"
+				});
+				jQuery(socialButtons).append(newButton);
+			}
+		});
+	},1000);
+});
